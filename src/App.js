e6e6670fd0d5b13ed188db2d88edbadac44a7eff
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-06 09:15:57 -0400

    add new button (HipsterButton) with different styles by extending existing button (DefaultButton)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 import BasicTitle from './components/BasicTitle';
-import { DefaultButton } from './components/Buttons'
+import { DefaultButton, HipsterButton } from './components/Buttons'
 
 function App() {
   return (
     <div style={{padding:'2rem'}}>
       <BasicTitle special>Styled Component</BasicTitle>
       <DefaultButton>Click Me</DefaultButton>
+      <HipsterButton>Click Me</HipsterButton>
     </div>
   );
 }
