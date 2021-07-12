f379e067e669d20e61386c35331357bf78348cdc
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-12 16:24:51 -0400

    add card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
-import ComplexTitle from './components/ComplexTitle'
-import AlternativeTitle from './components/AlternativeTitle'
+import Card from './components/Card'
 
 function App() {
   return (
     <div style={{padding:'2rem'}}>
-      <ComplexTitle title="more complex title"/>
-      <AlternativeTitle title="alternative title"/>
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = () => {
+  return (
+    <StyledCard>
+      <img src="https://dl.airtable.com/.attachmentThumbnails/0be1af59cf889899b5c9abb1e4db38a4/d631ac52" alt='product'></img>
+      <footer>
+        <h4>product name</h4>
+        <p>$555</p>
+      </footer>
+    </StyledCard>
+  )
+}
+
+const StyledCard = styled.article`
+width: 90vw;
+max-width: 300px;
+background-color: var(--white);
+border-radius: 0.25rem;
+img {
+  width: 100%;
+  border-radius: 0.25rem .25rem 0 0;
+}
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  h4{
+  text-transform: capitalize;
+  }
+  p{
+    color: var(--primary);
+    font-weight: 700;
+  }
+}
+`
+
+export default Card
