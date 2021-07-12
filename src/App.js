437e31f914f7efba7e6f8661380711f61bd953a2
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-12 16:39:44 -0400

    add global-styles.js, remove index.css imoprt in index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import Card from './components/Card'
+import GlobalStyles from './global-styles';
 
 function App() {
   return (
     <div style={{padding:'2rem'}}>
+      <GlobalStyles />
       <Card />
     </div>
   );
