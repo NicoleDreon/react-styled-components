64166f096787274c2ed0f8a1210111548d30ba4a
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-12 16:54:15 -0400

    add theme and useState to toggle base/dark theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import styled from 'styled-components'
+import {useState} from 'react'
+import styled, {ThemeProvider} from 'styled-components'
 import Card from './components/Card'
 import GlobalStyles from './global-styles';
 
+// typically would be in a seprate file
+const BaseTheme = {
+  color:'#222',
+  background:'#fff',
+}
+
+const DarkTheme = {
+  color:'#fff',
+  background:'#222',
+}
+
+const Container = styled.div`
+padding:2rem;
+color:${(props)=>props.theme.color};
+background:${(props)=>props.theme.background};
+`
+
 function App() {
+  const [baseTheme, setTheme] = useState(true);
+
+  const toggleTheme = ()=>{
+    setTheme(!baseTheme)
+  }
   return (
-    <div style={{padding:'2rem'}}>
+    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
       <GlobalStyles />
-      <Card />
-    </div>
+      <Container>
+        <h1>Hello World</h1>
+        <button className="btn" onClick={toggleTheme}>toggle</button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
