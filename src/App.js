0ae2532c23333758999c05f12635e18be25f9115
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-13 09:12:34 -0400

    add css prop to button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {HipsterButton} from './components/Buttons'
 function App() {
   return (
    <div style={{padding:'2rem'}}>
-     <HipsterButton as='a' href="https://perficient.udemy.com/course/styled-components-tutorial-and-project-course/learn/lecture/26538166#overview">click me</HipsterButton>
+     <HipsterButton css={`color:green;`} as='a' href="https://perficient.udemy.com/course/styled-components-tutorial-and-project-course/learn/lecture/26538166#overview">click me</HipsterButton>
      <HipsterButton>click me</HipsterButton>
      <HipsterButton>click me</HipsterButton>
      <div css={`color:green`}>
