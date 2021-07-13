012ea8112ae8f0d8b39edaa2219c7ac66f2a52f2
Nicole Dreon <nicole.dreon@perficient.com>
2021-07-13 10:41:57 -0400

    add list of products and conditional styles to prices

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import styled from 'styled-components/macro'
-import List from './components/List'
+import Products from './components/Products'
 
 function App() {
   return (
    <div style={{padding:'2rem'}}>
-     <List />
+     <Products />
    </div>
   );
 }
